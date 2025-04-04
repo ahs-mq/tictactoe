bc3bf7b3aaf571f1c1c8bd938f042f1ac68c616b
ahs-mq <141071955+ahs-mq@users.noreply.github.com>
2025-04-04 20:44:03 +0400

    refactored code to modify dom elements

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 let board = new Array(9).fill(null); //creates an array with 9 empty "cells"
 let turn = 0;
 let grid = document.querySelectorAll(".grid");
+let player1 = '';
+let player2 = '';
+let winner = document.querySelector(".winner");
+let score1 = document.querySelector(".score1");
+let score2 = document.querySelector(".score2");
+let name1 = document.querySelector(".name1");
+let name2 = document.querySelector(".name2");
+let reset = document.querySelector(".restart");
+let sc1 = 0;
+let sc2 = 0;
 
-(() => {
+
+
+(()=>{
+    player1 = prompt('Enter Name for Player 1');
+    name1.textContent = `Name: ${player1}`;
+    player2 = prompt('Enter Name for Player 2');
+    name2.textContent = `Name: ${player2}`;
+})();
+
+(()=>{
     for (let i = 0; i < grid.length; i++) {
         grid[i].setAttribute('value', i);
         grid[i].addEventListener("click", () => {
             playGame(i)
         });
     }
+    reset.addEventListener("click",()=>{
+        board = new Array(9).fill(null);
+        for(let j=0 ; j <grid.length;j++){
+            grid[j].textContent = '';
+            winner.textContent = 'Game Restarted';
+        }
+    })
 })();
 
-
+function resetGame(){
+    board = new Array(9).fill(null);
+        for(let j=0 ; j <grid.length;j++){
+            grid[j].textContent = '';
+        }
+}
 
 function playerChoice(choice){
     choice = Number(choice);
@@ -28,7 +59,6 @@ function playerChoice(choice){
             return 'o';
         }
     }else {
-        window.alert("Invalid play");
         return null;
     }
    
@@ -42,24 +72,33 @@ function verifyWin(board,player){
     for (let i = 0; i < winCombos.length; i++){
         let combo = winCombos[i]; //iterates over all possible win combos
         if (board[combo[0]] == player && board[combo[1]] == player && board[combo[2]] == player && (player == 'x' || player == 'o')){
-            console.log(board);
             return true;
             
-        }else {
-            console.log(board);
-            return false;
         }
-    }
+    } return false;
 }
 
 function playGame(index){
+    winner.textContent = '';
     let player = playerChoice(index);
     if (player){
         win = verifyWin(board,player);
     }if (win){
-        window.alert(`${player === 'x' ? 1 : 2} Wins!`)
+        if (player == 'x'){
+            sc1++;
+            winner.textContent = `${player1} wins!`;
+            score1.textContent = `Score: ${sc1}`;
+            resetGame();
+        }
+        else{
+            sc2++;
+            winner.textContent = `${player2} wins!`;
+            score2.textContent = `Score: ${sc2}`;
+            resetGame();
+        }
     }else if(!board.includes(null)){
-        window.alert("tie")
+        winner.textContent = "Tie";
+        resetGame();
     }
 }
     
